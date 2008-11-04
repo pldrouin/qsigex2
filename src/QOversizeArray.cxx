0a2a140bd3ebe2d98e84e7eb7635747135ffc939
pldrouin <pldrouin@234f9065-3419-4277-ac21-66abb24dcc04>
2008-11-04 22:05:23 +0000

    -Changed format when printing priorities in QOversizeArray.

--- src/QOversizeArray.cxx
+++ src/QOversizeArray.cxx
@@ -81,16 +81,6 @@ void QOversizeArray::CloseFile()
   if(fFDesc) {
     pthread_mutex_lock(&fILMutex);
 
-    /*printf("\nPriorities:\n");
-    Int_t i;
-    Float_t fbuf=0;
-    pthread_mutex_lock(&fPriorityMutex);
-    for(i=0; i<fInstances.Count(); i++) fbuf+=fInstances.GetArray()[i]->fAPriority;
-    for(i=0; i<fInstances.Count(); i++) {
-      printf("Array '%s': %7.3f\n",fInstances.GetArray()[i]->fFilename.Data(),fbuf?fInstances.GetArray()[i]->fAPriority*100./fbuf:-1.);
-    }
-    pthread_mutex_unlock(&fPriorityMutex);*/
-
     Int_t idx=fInstances.FindFirst(this);
     fICumulPriority.Del(idx);
     fInstances.Del(idx);
@@ -479,7 +469,7 @@ void QOversizeArray::PrintPriorities()
   for(i=0; i<fInstances.Count(); i++) fbuf+=fInstances.GetArray()[i]->fAPriority;
 
   for(i=0; i<fInstances.Count(); i++) {
-    printf("Array '%s': %7.3f\n",fInstances.GetArray()[i]->fFilename.Data(),fbuf?fInstances.GetArray()[i]->fAPriority*100./fbuf:-1.);
+    printf("Array '%s': %7.5e\n",fInstances.GetArray()[i]->fFilename.Data(),fbuf?fInstances.GetArray()[i]->fAPriority*100./fbuf:-1.);
   }
   pthread_mutex_unlock(&fPriorityMutex);
   pthread_mutex_unlock(&fILMutex);
