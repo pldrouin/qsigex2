6b08a3e872b6fbd2b5a729ca1bd9265bef4e001c
pldrouin <pldrouin@234f9065-3419-4277-ac21-66abb24dcc04>
2009-02-09 00:29:43 +0000

    -Deleted unused variable in QTHNDL_Dict_cxx.h

--- src/QTHNDL_Dict_cxx.h
+++ src/QTHNDL_Dict_cxx.h
@@ -3,7 +3,6 @@ template <typename U> void QTHNDL<U>::Streamer(TBuffer &R__b)
   // Stream an object of class QTHNDL<U>.
 
   UInt_t R__s, R__c;
-  Long64_t li;
 
   if (R__b.IsReading()) {
     Version_t R__v = R__b.ReadVersion(&R__s, &R__c); if (R__v) { }
