20c0f0d9926353803edad45fc016f9bdc073a7f3
pldrouin <pldrouin@234f9065-3419-4277-ac21-66abb24dcc04>
2009-03-04 20:09:00 +0000

    -Update htmlgen.C to work with the new QSigEx libraries.

--- src/htmlgen.C
+++ src/htmlgen.C
@@ -4,7 +4,8 @@
   gSystem->Load("libHist.so");
   gSystem->Load("libRIO.so");
   gSystem->Load("libMatrix.so");
-  gSystem->Load("libqsigex2.so");
+  gSystem->Load("libqsigex-1.so");
+  gSystem->Load("libqsigex-2.so");
   gEnv->SetValue("Root.Html.Root","http://root.cern.ch/root/html");
 
   THtml html;
