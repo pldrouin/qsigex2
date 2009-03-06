9c968130fdf8e5bf4b4e3e9fe52fd591a0e56a26
pldrouin <pldrouin@234f9065-3419-4277-ac21-66abb24dcc04>
2009-03-06 03:49:59 +0000

    -Fixed the name of the qsigex library in htmlgen.C

--- src/htmlgen.C
+++ src/htmlgen.C
@@ -4,8 +4,7 @@
   gSystem->Load("libHist.so");
   gSystem->Load("libRIO.so");
   gSystem->Load("libMatrix.so");
-  gSystem->Load("libqsigex-1.so");
-  gSystem->Load("libqsigex-2.so");
+  gSystem->Load("libqsigex2.so");
   gEnv->SetValue("Root.Html.Root","http://root.cern.ch/root/html");
 
   THtml html;
