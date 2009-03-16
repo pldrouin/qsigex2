cba36f306d0b8220dd4cdc1bbe58ac5e37f53405
pldrouin <pldrouin@234f9065-3419-4277-ac21-66abb24dcc04>
2009-03-16 20:15:36 +0000

    -A bug in the last commit has been fixed.

--- src/QTypes.h
+++ src/QTypes.h
@@ -7,9 +7,9 @@
 
 namespace QTypes {
     enum TypeID {kBool, kChar, kUChar, kShort, kUShort, kInt, kUInt, kFloat, kDouble, kLong64, kULong64};
+    UInt_t GetTypeIDSize(const Int_t &type);
     void* AllocType(const Int_t &type){return malloc(GetTypeIDSize(type));}
     Int_t GetNameTypeID(const TString adesc, TString *name=NULL);
-    UInt_t GetTypeIDSize(const Int_t &type);
     const Char_t* GetTypeName(const Int_t &type);
     const Char_t* GetTypeSName(const Int_t &type);
 }
